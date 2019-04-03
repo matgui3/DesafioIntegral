567686770646390f477948d6b6830eb40ca07166
Unknown <guilhermerd6@hotmail.com>
2019-04-02 22:21:30 -0300

    Subindo as configurações iniciais do Spring Boot.

---
+++ src/main/java/com/DesafioIntegral/DesafioIntegral/DesafioIntegralApplication.java
@@ -0,0 +1,13 @@
+package com.DesafioIntegral.DesafioIntegral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioIntegralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioIntegralApplication.class, args);
+	}
+
+}
