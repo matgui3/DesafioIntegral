3be7b629efaea11c2aa89078efebd3779ca089f7
Unknown <guilhermerd6@hotmail.com>
2019-04-07 19:38:47 -0300

    Removendo importações desnecessárias do  Cozinheiro Repository.

--- src/main/java/com/DesafioIntegral/DesafioIntegral/cozinheiro/CozinheiroRepository.java
+++ src/main/java/com/DesafioIntegral/DesafioIntegral/cozinheiro/CozinheiroRepository.java
@@ -1,10 +1,6 @@
 package com.DesafioIntegral.DesafioIntegral.cozinheiro;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
-
-import com.DesafioIntegral.DesafioIntegral.prato.Prato;
 
 public interface CozinheiroRepository extends CrudRepository<Cozinheiro, Long>{
 	
